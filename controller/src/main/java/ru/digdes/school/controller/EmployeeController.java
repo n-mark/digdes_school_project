6d64c02395a7b9ec84a1b8c4e00a9a9725c8f065
Nikita Markovskiy <markovskinikita@gmail.com>
2023-05-12 21:02:31 +0300

    homework 2 fixes

--- controller/src/main/java/ru/digdes/school/controller/EmployeeController.java
+++ controller/src/main/java/ru/digdes/school/controller/EmployeeController.java
@@ -2,14 +2,12 @@ package ru.digdes.school.controller;
 
 import ru.digdes.school.dto.EmployeeDto;
 import ru.digdes.school.model.Employee;
-import ru.digdes.school.security.SecurityConfig;
 import ru.digdes.school.service.CommonService;
 import ru.digdes.school.service.EmployeeService;
 
 import java.util.logging.Logger;
 
 public class EmployeeController {
-    SecurityConfig securityConfig = new SecurityConfig();
     Logger logger = Logger.getLogger(EmployeeController.class.getName());
 
     public EmployeeDto getOne(Long id) {
