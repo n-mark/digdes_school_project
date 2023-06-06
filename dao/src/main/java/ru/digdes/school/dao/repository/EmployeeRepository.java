33e87bc5cc32fa69a769065b225c03aca7f5dc42
Nikita Markovskiy <markovskinikita@gmail.com>
2023-06-06 13:15:08 +0300

    before hw7

--- dao/src/main/java/ru/digdes/school/dao/repository/EmployeeRepository.java
+++ dao/src/main/java/ru/digdes/school/dao/repository/EmployeeRepository.java
@@ -2,10 +2,8 @@ package ru.digdes.school.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import ru.digdes.school.model.employee.Employee;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
